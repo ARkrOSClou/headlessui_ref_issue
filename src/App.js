7d8db67b9d3741a4886a9caf048b51d637a16307
Maxim Khmara <arkrosclou@gmail.com>
2022-05-27 21:15:09 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,17 @@
-import { Transition } from '@headlessui/react'
-import { useEffect, useRef } from 'react'
+import { Menu } from '@headlessui/react'
+import React from "react";
+
+const Item = () => <div>Item</div>
 
 function App() {
-  const refParent = useRef(null)
-  const refChild = useRef(null)
-
-  useEffect(() => {
-    console.log('refParent', refParent.current)
-    console.log('refChild', refChild.current)
-
-    refParent.current && (refParent.current.style.color = "tomato")
-    refChild.current && (refChild.current.style.color = "orange")
-  }, [])
-
   return (
-    <>
-      <Transition ref={refParent} show={true}>
-        <h3>Parent</h3>
-        <Transition.Child
-          ref={refChild}
-          enter="transition ease-in-out duration-300 transform"
-          enterFrom="-translate-x-full"
-          enterTo="translate-x-0"
-          leave="transition ease-in-out duration-300 transform"
-          leaveFrom="translate-x-0"
-          leaveTo="-translate-x-full"
-        >
-          <h3>Child</h3>
-        </Transition.Child>
-      </Transition>
-    </>
+	  <Menu>
+		  <Menu.Items static={true}>
+			  <Menu.Item>
+				  <Item />
+			  </Menu.Item>
+		  </Menu.Items>
+	  </Menu>
   )
 }
 
