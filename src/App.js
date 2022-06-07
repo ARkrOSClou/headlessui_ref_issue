7fd4ce25240c7f8cc21de1360765ed74a7d60d2d
Maxim Khmara <arkrosclou@gmail.com>
2022-06-07 15:45:26 +0300

    transition

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
-import { Menu } from '@headlessui/react'
-import React from "react";
+import React, { useState } from "react";
+import { Transition } from "@headlessui/react";
 
-const Item = () => <div>Item</div>
+const Comp = (props) => {
+	const [isShowing, setIsShowing] = useState(false)
+	return (
+		<div className='flex'>
+			<button
+				onClick={() => setIsShowing((isShowing) => !isShowing)}
+				className='bg-[orange] p-2'
+			>
+				Toggle
+			</button>
 
-function App() {
-  return (
-	  <Menu>
-		  <Menu.Items static={true}>
-			  <Menu.Item>
-				  <Item />
-			  </Menu.Item>
-		  </Menu.Items>
-	  </Menu>
-  )
+			<div className="grow relative">
+				<Transition
+					show={isShowing}
+					unmount={props.static}
+					// appear={true}
+					enter="transition duration-[1500ms]"
+					enterFrom="-translate-y-full"
+					enterTo="translate-y-0"
+					leave="transition duration-[1500ms]"
+					leaveFrom="translate-y-0"
+					leaveTo="-translate-y-full"
+					className="absolute inset-0 h-[200px] bg-[tomato] transition will-change-transform"
+				>
+					{'menu'}
+				</Transition>
+			</div>
+		</div>
+	)
 }
 
-export default App
+export default function App() {
+	return (
+		<div className='flex'>
+			<div className="grow">
+				<Comp static={true} />
+				works fine
+			</div>
+			<div className="grow">
+				<Comp static={false} />
+				broken
+			</div>
+		</div>
+	);
+}
